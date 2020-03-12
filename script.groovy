54ca6cf3fd2041257f87b19c517658965d6fabbd
Faisal <sfaisalawan84@gmail.com>
2020-03-12 17:46:15 +0000

    Groovy Script Updated to build Project

--- script.groovy
+++ script.groovy
@@ -1,8 +1,7 @@
 def buildApp() {
-	echo "Starting Application Build with dotnet."
-	echo "Checking out the directory"
-	print "cd C:\Users\Faisal\Dropbox\University\Projects on GitHub\DevOps\DevOps".execute().text
-	echo "Running dotnet msbuild"
-	print "dotnet msbuild DevOps.sln".execute().text
+	echo "Starting Application Build with dotnet..."
+	echo "Checking out the directory and building the project..."
+	'cmd /c dotnet build "C:/Users/Faisal/Dropbox/University/Projects on GitHub/DevOps/DevOps"'.execute().text
+	echo "PROCESSED"
 }
 return this
